6226a849b389248b42ea6c66fcd6f90cc7cc3f92
Parvathi Pai <45047756+ParvathiRPai@users.noreply.github.com>
2019-11-23 21:02:53 -0800

    Update: Project_Proposal.pdf notebooks

--- notebooks/Users/pava@uw.edu/MediCare.scala
+++ notebooks/Users/pava@uw.edu/MediCare.scala
@@ -1,14 +1,2 @@
 // Databricks notebook source
 val loan=sc.textFile("dbfs:/autumn_2019/pava/project/medicare.xlsx")
-
-// COMMAND ----------
-
-import org.apache.spark.sql.functions._
-
-// COMMAND ----------
-
-loan.show()
-
-
-// COMMAND ----------
-
